aa4c35506a7674d77047bf0c5f51150764a8d429
AbhiModszYT <127215382+AbhiModszYT@users.noreply.github.com>
2023-03-30 02:49:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/Arch3xpert/stringhackbot.git")
+os.system("git clone https://github.com/AbhiModszYT/stringhackbot.git")
 os.system("cd stringhackbot && python bot.py")
