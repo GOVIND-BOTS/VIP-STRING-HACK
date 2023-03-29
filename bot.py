2e1214f8a9f3fc6f509b19e933966594942a321f
AbhiModszYT <127215382+AbhiModszYT@users.noreply.github.com>
2023-03-30 03:10:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 # COPYRIGHT © 2021-22 BY LEGENDX22 🔥
 # NOW PUBLIC BY LEGENDX
 import os
-os.system("pip install Telethon==1.21.1")
+os.system("pip install Telethon==1.27.0")
 from telethon import TelegramClient, events, functions, types
 api_id = os.environ.get("APP_ID")
 import os, asyncio
