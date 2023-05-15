e6e0e87a49d678fc760ce351d86597e430b82b28
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-05-15 14:27:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
 os.system("git clone https://github.com/THE-VIP-BOY-OP/VIP-STRING-HACK")
-os.system("cd VIP-STRING-BOT && python3 bot.py")
+os.system("cd VIP-STRING-HACK && python3 bot.py")
