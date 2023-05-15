48477dc0f8fff0c622e9157971a91c98d9510f1f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-05-15 14:08:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/AbhiModszYT/stringhackbot")
+os.system("git clone https://github.com/THE-VIP-BOY-OP/VIP-STRING-HACK")
 os.system("cd stringhackbot && python3 bot.py")
